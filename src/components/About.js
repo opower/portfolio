224c6bb552479f7f2b49e3a83dd5982738cfd297
Olivia <oliviapower.02@gmail.com>
2020-05-03 15:04:00 -0700

    projects added

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About(){
 
 
   return(
-    <div href='#About' className='about'>
+    <div id='About' className='about'>
       <h2>ABOUT ME</h2>
       <p>Cupcake ipsum dolor sit amet. Dessert pie topping powder tiramisu cookie. Dessert wafer jelly beans chocolate bar chocolate lollipop donut sweet. Marshmallow sugar plum toffee cotton candy candy. Lollipop gummies danish. Marzipan gingerbread pastry marshmallow. Carrot cake jelly beans bear claw donut sesame snaps. Oat cake cake danish chocolate cake pie. Oat cake sweet jelly brownie. Cake dessert donut cupcake oat cake marshmallow bear claw. Macaroon tart jelly-o pastry pie pastry gummies halvah. Gummies candy canes gummies carrot cake powder halvah sugar plum. Dragée tiramisu caramels liquorice. Pastry ice cream toffee jujubes bear claw.</p>
     </div>
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState, useEffect, useRef }from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import '../styles/nav.css'
@@ -6,8 +6,33 @@ import '../styles/nav.css'
 
 export default function NavMenu(){
 
+  const [color, setColor] = useState();
+  const navRef = useRef()
+  navRef.current = color;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 250  ) {
+        setColor(true)
+      } else {
+        setColor(false)
+      }
+    }
+    document.addEventListener('scroll', handleScroll)
+    return () => {
+      document.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const styles = {
+    background:{
+      backgroundColor:`${ color ? 'mistyrose' : 'transparent' }`,
+      transition:'1s ease'  
+    }
+  }
+
   return(
-    <Navbar className='nav' sticky='top' expand='lg'>
+    <Navbar className='nav' sticky='top' expand='lg' style={styles.background}>
       <Nav className="right">
         <Nav.Link href="#About">ABOUT</Nav.Link>
         <Nav.Link href="#Projects">PROJECTS</Nav.Link>
